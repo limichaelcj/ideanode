16c8c773304cdb6e55dedbf39c429ccddd6b420a
mcli830 <39263631+mcli830@users.noreply.github.com>
2018-08-06 14:53:33 -0500

    Convert presentationals (Control/Workspace) to functional components

--- src/components/Control.js
+++ src/components/Control.js
@@ -3,21 +3,16 @@ import React, { Component } from 'react';
 //css
 import '../css/Control.css';
 
-class Control extends Component {
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return(
-      <div id='Control'>
-        <button
-          id='button-addNode'
-          onClick={this.props.addNode}
-        >Add Node
-        </button>
-      </div>
-    );
-  }
+const Control = (props)=> {
+  return(
+    <div id='Control'>
+      <button
+        id='button-addNode'
+        onClick={props.addNode}
+      >Add Node
+      </button>
+    </div>
+  );
 }
 
 export default Control;
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -3,21 +3,16 @@ import React, { Component } from 'react';
 //css
 import '../css/Workspace.css';
 
-class Workspace extends Component {
-  constructor(props){
-    super(props)
-  }
-  render(){
-    return(
-      <div id='Workspace'>
-        <svg id='Linkspace' className='Linkspace' x='0' y='0' width='2000' height='1000'>
-        </svg>
-        <div id='Nodespace' className='Nodespace'>
-          {this.props.file}
-        </div>
+const Workspace = (props) => {
+  return(
+    <div id='Workspace'>
+      <svg id='Linkspace' className='Linkspace' x='0' y='0' width='2000' height='1000'>
+      </svg>
+      <div id='Nodespace' className='Nodespace'>
+        {props.file}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Workspace;
